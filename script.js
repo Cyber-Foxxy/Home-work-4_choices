c71fc5bf7d1a4a962e3c169a7df466a8e801e27a
Cyber-Foxxy <bluenosepittiezephyr2019@gmail.com>
2026-02-11 13:04:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let chosenGame = "";
 
 // Update Foxy's speech bubble
-function updateBubble(Yeah, that sounds fun! lets play!) {
-  document.getElementById("bubble").innerHTML = lets play!;
+function updateBubble(text) {
+  document.getElementById("bubble").innerHTML = text;
 }
 
 function chooseGame(game) {
   chosenGame = game;
 
-  let storyText = document.getElementById("Lets help Foxy on her day off! Foxy is working hard in college to achive her goals, but today she finally has some time off from her busy schedual, and wants to play a game with you :) ");
+  let storyText = document.getElementById("story-text");
   let choices = document.getElementById("choices");
 
   if (game === "tic") {
     storyText.innerHTML = "Foxy picks Tic‑Tac‑Toe! Let's go play!";
-    updateBubble("Tic‑Tac‑Toe sounds fun!");
+    updateBubble("Yeah, that sounds fun! Let's play Tic‑Tac‑Toe!");
     choices.innerHTML =
       '<a href="https://playtictactoe.org/" target="_blank"><button>Play Tic‑Tac‑Toe</button></a>' +
       '<button onclick="returnToFoxy()">Return to Foxy</button>';
@@ -21,7 +21,7 @@ function chooseGame(game) {
 
   if (game === "checkers") {
     storyText.innerHTML = "Foxy chooses Checkers! Time to jump some pieces!";
-    updateBubble("Checkers? Ohhh I love strategy!");
+    updateBubble("Ooooh Checkers! I love strategy games!");
     choices.innerHTML =
       '<a href="https://www.online-checkers.com/" target="_blank"><button>Play Checkers</button></a>' +
       '<button onclick="returnToFoxy()">Return to Foxy</button>';
@@ -29,9 +29,9 @@ function chooseGame(game) {
 
   if (game === "cards") {
     storyText.innerHTML = "Foxy decides to play Cards! Shuffle up!";
-    updateBubble("Cards! I hope I get a good hand!");
+    updateBubble("Cards? Shuffle shuffle! Let's see if I get a good hand!");
     choices.innerHTML =
-      '<a href="https://solitaired.com/" target="_blank"><button>Play Cards</button></a>' +
+      '<a href="https://www.solitr.com/" target="_blank"><button>Play Cards</button></a>' +
       '<button onclick="returnToFoxy()">Return to Foxy</button>';
   }
 }
@@ -48,7 +48,7 @@ function returnToFoxy() {
 
 function restart() {
   document.getElementById("story-text").innerHTML =
-    "Foxy finally has a day off work! She wants to play a game today… but she can’t decide which one!";
+    "Let's help Foxy on her day off! Foxy is working hard in college to achieve her goals, but today she finally has some time off from her busy schedule and wants to play a game with you :)";
 
   updateBubble("Hi again! Want to pick another game?");
 
@@ -58,41 +58,6 @@ function restart() {
     '<button onclick="chooseGame(\'cards\')">Cards</button>';
 }
 
-  if (game === "checkers") {
-    storyText.innerHTML =
-      "Foxy chooses Checkers! Time to jump some pieces!";
-    choices.innerHTML =
-      '<a href="https://www.online-checkers.com/" target="_blank"><button>Play Checkers</button></a>' +
-      '<button onclick="returnToFoxy()">Return to Foxy</button>';
-  }
-
-  if (game === "cards") {
-    storyText.innerHTML =
-      "Foxy decides to play Cards! Shuffle up!";
-    choices.innerHTML =
-      '<a href="https://solitaired.com/" target="_blank"><button>Play Cards</button></a>' +
-      '<button onclick="returnToFoxy()">Return to Foxy</button>';
-  }
-}
-
-// After returning from the game
-function returnToFoxy() {
-  let storyText = document.getElementById("story-text");
-  let choices = document.getElementById("choices");
-
-  storyText.innerHTML =
-    "Oh boy, that was such a fun game! Thank you for playing, and helping me decide too!";
-
-  choices.innerHTML =
-    '<button onclick="restart()">Play Again</button>';
-}
-
-// Restart the adventure
-function restart() {
-  document.getElementById("story-text").innerHTML =
-    "Foxy finally has a day off work! She wants to play a game today… but she can’t decide which one!";
-  
-  document.getElementById("choices").innerHTML =
     '<button onclick="chooseGame(\'tic\')">Tic‑Tac‑Toe</button>' +
     '<button onclick="chooseGame(\'checkers\')">Checkers</button>' +
     '<button onclick="chooseGame(\'cards\')">Cards</button>';

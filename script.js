10d7fb0d050bb502448f4bba7e22e76526c7b6c4
Cyber-Foxxy <bluenosepittiezephyr2019@gmail.com>
2026-02-11 13:28:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function updateBubble(text) {
   document.getElementById("bubble").innerHTML = text;
 }
 
+// Open game links in a new tab
+function openGame(url) {
+  window.open(url, "_blank");
+}
+
 function chooseGame(game) {
   chosenGame = game;
 
@@ -12,10 +17,10 @@ function chooseGame(game) {
   let choices = document.getElementById("choices");
 
   if (game === "tic") {
-    storyText.innerHTML = "Foxy picks Tic-Tac-Toe! Let's go play!";
-    updateBubble("Tic-Tac-Toe sounds fun!");
+    storyText.innerHTML = "Foxy picks Tic‑Tac‑Toe! Let's go play!";
+    updateBubble("Tic‑Tac‑Toe sounds fun!");
     choices.innerHTML =
-      '<button onclick="openGame(\'https://playtictactoe.org/\')">Play Tic-Tac-Toe</button>' +
+      '<button onclick="openGame(\'https://playtictactoe.org/\')">Play Tic‑Tac‑Toe</button>' +
       '<button onclick="returnToFoxy()">Return to Foxy</button>';
   }
 
@@ -32,6 +37,28 @@ function chooseGame(game) {
     updateBubble("Cards! I hope I get a good hand!");
     choices.innerHTML =
       '<button onclick="openGame(\'https://www.solitr.com/\')">Play Cards</button>' +
-      '<button onclick="retur
-;
+      '<button onclick="returnToFoxy()">Return to Foxy</button>';
+  }
+}
+
+function returnToFoxy() {
+  document.getElementById("story-text").innerHTML =
+    "Oh boy, that was such a fun game! Thank you for playing, and helping me decide too!";
+
+  updateBubble("That was awesome! Thanks for playing with me!");
+
+  document.getElementById("choices").innerHTML =
+    '<button onclick="restart()">Play Again</button>';
+}
+
+function restart() {
+  document.getElementById("story-text").innerHTML =
+    "Let's help Foxy on her day off! Foxy is working hard in college to achieve her goals, but today she finally has some time off from her busy schedule and wants to play a game with you :)";
+
+  updateBubble("Hi again! Want to pick another game?");
+
+  document.getElementById("choices").innerHTML =
+    '<button onclick="chooseGame(\'tic\')">Tic‑Tac‑Toe</button>' +
+    '<button onclick="chooseGame(\'checkers\')">Checkers</button>' +
+    '<button onclick="chooseGame(\'cards\')">Cards</button>';
 }
